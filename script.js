9ea5992cae05427aa05dc676fcdb3293fd670719
Ceejay-777 <covenantcrackslord01@gmail.com>
2022-11-18 22:07:47 +0100

    commit

--- script.js
+++ script.js
@@ -1,30 +1,35 @@
-var active = false;
-$('.division').click(function() {
-    active = !active;
-    if (active) {
-        $('.division').children('.question').css('font-weight', '400')
-        $(this).children('.question').css('font-weight', '700');
-        $('.division').children('.answer').hide(300);
-        $(this).children('.answer').show(300);
-        $('.division').children('.question').children('img').css('transform', '');
-        $(this).children('.question').children('img').css({
-            'transform' : 'rotateZ(180deg)',
-            'transition' : '0.3s ease-in-out'});
-    } else {
-        $(this).children('.question').css('font-weight', '400');
-        $(this).children('.answer').hide(300);
-        $(this).children('.question').children('img').css('transform', '');
-        $(this).mouseenter(function() {
-            $(this).css({
-                'color' : 'var(--softRed)',
-                'cursor' : 'pointer'
-            });
+$('document').ready(function() {
+    $('.division').each(function(index) {
+        $(this).data('active', false);
+        $(this).data('index', index);
+    });
+
+    $('.division').click(function() {
+        var clicked = $(this);
+        $(this).data('active', !$(this).data('active'));
+
+        $('.division').each(function() {
+            if($(this).data('index') === clicked.data('index')) {
+                
+            } else {
+                $(this).data('active', false);
+            };
         });
-        $(this).mouseleave(function() {
-            $(this).css({
-                'color' : '',
-                'cursor' : ''
-            });
-        });
-    };
-});
+
+        if ($(this).data('active')) {
+            $('.division').children('.question').css('font-weight', '400');
+            $(this).children('.question').css('font-weight', '700');
+            $('.division').children('.answer').hide(300);
+            $(this).children('.answer').show(300);
+            $('.division').children('.question').children('img').css('transform', '');
+            $(this).children('.question').children('img').css({
+                'transform' : 'rotateZ(180deg)',
+                'transition' : '0.3s ease-in-out'});
+        } else {
+            $(this).children('.question').css('font-weight', '400');
+            $(this).children('.answer').hide(300);
+            $(this).children('.question').children('img').css('transform', '');
+        };
+    });
+})
+
